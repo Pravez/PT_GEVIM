1be16d3f072d7bd3d459374bfcddcb91beb1d89a
paubreton <paul.ravezb@gmail.com>
2015-02-03 11:22:42 +0100

    Commit de base

--- core/src/View/window.java
+++ core/src/View/window.java
@@ -4,91 +4,89 @@
 
 package View;
 
+import sun.rmi.server.Activation;
+
+import java.awt.*;
 import java.lang.*;
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.*;
 
-public class window {
+public class window extends JFrame {
     private int width;
     private int height;
-    private JFrame mainWindow;
 
 
     public window(int w, int h) {
-        /* Création des composants */
-        mainWindow = new JFrame("PT-Modélisation de graphe");
+    /* Création des composants */
+        width = w;
+        height = h;
+        this.setTitle("PT - Modélisation de graphe");
+        this.setSize(width, height);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-
-
-
-
-
-
-
-
-    }
-
-    final JButton clic = new JButton("Cliquer");
-    JPanel panel1= new JPanel();
+        final JButton clic = new JButton("Cliquer");
+        JPanel pan1= new JPanel();
+        pan1.setBackground(Color.WHITE);
     /* Bar de menu */
-    JMenuBar menu_bar1 = new JMenuBar();
+        JMenuBar mainMenu = new JMenuBar();
     /* différents menus */
-    JMenu menu1 = new JMenu("Fichier");
-    JMenu menu2 = new JMenu("Edition");
+        JMenu menuFichier = new JMenu("Fichier");
+        JMenu menuEdition = new JMenu("Edition");
     /* differents choix de chaque menu */
-    JMenuItem demarrer = new JMenuItem("Démarrer");
-    JMenuItem fin = new JMenuItem("Fin");
-    JMenuItem annuler = new JMenuItem("Annuler");
-    JMenuItem copier = new JMenuItem("Copier");
-    JMenuItem coller = new JMenuItem("Coller");
+        JMenuItem demarrer = new JMenuItem("Démarrer");
+        JMenuItem fin = new JMenuItem("Fin");
+        JMenuItem Nouveau = new JMenuItem("Nouveau");
+        JMenuItem Enregistrer = new JMenuItem("Enregistrer");
+        JMenuItem Charger = new JMenuItem("Charger");
+        JMenuItem annuler = new JMenuItem("Annuler");
+        JMenuItem copier = new JMenuItem("Copier");
+        JMenuItem coller = new JMenuItem("Coller");
 
 		/* Ajout de composants aux conteneurs  */
-    clic.setEnabled(false);
-    panel1.add(clic);
-    frame1.getContentPane().add(panel1,"South");
+        clic.setEnabled(false);
+        pan1.add(clic);
+        this.getContentPane().add(pan1);
 				/* Ajouter les choix au menu  */
-    menu1.add(demarrer);
-    menu1.add(fin);
-    menu2.add(annuler);
-    menu2.add(copier);
-    menu2.add(coller);
+        menuFichier.add(demarrer);
+        menuFichier.add(fin);
+        menuFichier.add(Nouveau);
+        menuFichier.add(Enregistrer);
+        menuFichier.add(Charger);
+
+        menuEdition.add(annuler);
+        menuEdition.add(copier);
+        menuEdition.add(coller);
 				/* Ajouter les menu sur la bar de menu */
-    menu_bar1.add(menu1);
-    menu_bar1.add(menu2);
+        mainMenu.add(menuFichier);
+        mainMenu.add(menuEdition);
 				/* Ajouter la bar du menu à la frame */
-    frame1.setJMenuBar(menu_bar1);
+        this.setJMenuBar(mainMenu);
+
 
 		/* Action réaliser par l'ihm */
 				/* clic sur le bouton clic */
-    clic.addActionListener(new ActionListener()
-    {
-        public void actionPerformed(ActionEvent e)
-        {
-            System.out.println("1 clic");
-        }
-    });
+        clic.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("1 clic");
+            }
+        });
 				/* clic sur le choix Démarrer du menu fichier */
-    demarrer.addActionListener(new ActionListener()
-    {
-        public void actionPerformed(ActionEvent e)
+        demarrer.addActionListener(new ActionListener()
         {
-            clic.setEnabled(true);
-        }
-    });
+            public void actionPerformed(ActionEvent e)
+            {
+                clic.setEnabled(true);
+            }
+        });
 				/* clic sur le choix Fin du menu fichier */
-    fin.addActionListener(new ActionListener()
-    {
-        public void actionPerformed(ActionEvent e)
+        fin.addActionListener(new ActionListener()
         {
-            clic.setEnabled(false);
-        }
-    });
-
-    frame1.setSize(200,200);
-    frame1.show();
-}
-}
-
+            public void actionPerformed(ActionEvent e)
+            {
+                clic.setEnabled(false);
+            }
+        });
+        this.setVisible(true);
+    }
 }
