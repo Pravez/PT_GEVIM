548639e0f53b555eeb853347a489aa232b0388dd
Corentin Davidenko <corentin.davidenko@etu.u-bordeaux.fr>
2015-02-04 09:39:22 +0100

    Ajout de méthode et variables dans graphe.
    Création de la classe vue Tab qui est un Jpanel, c'est là qu'il faut faire l'affichage et l'édition du graphe

---
+++ core/src/View/Tab.java
@@ -0,0 +1,19 @@
+package View;
+
+import data.Graph;
+
+import javax.swing.*;
+
+/**
+ * Created by Corentin Davidenko on 04/02/15.
+ */
+
+
+public class Tab extends JPanel {
+
+    private Graph graph;
+    /*
+    A partir du graphe dessiner tout ce qu'il contient.
+     */
+
+}
--- core/src/data/Graph.java
+++ core/src/data/Graph.java
@@ -1,7 +1,8 @@
 package data;
 
 import javax.swing.undo.UndoManager;
-import java.util.List;
+import java.awt.*;
+import java.util.ArrayList;
 
 /**
  * Created by Corentin Davidenko on 04/02/15.
@@ -10,19 +11,29 @@ public class Graph {
 
     protected UndoManager undo = new UndoManager();
 
-    public List<Vertex> getVertexes() {
+    private ArrayList<Edge> edges;
+    private ArrayList<Vertex> vertexes;
+
+    private ArrayList<Edge> selectedEdges;
+    private ArrayList<Vertex> selectedVertexes;
+    private String name;
+    private String file;
+    private Color defaultColor;
+    private int defaultThickness;
+
+    public ArrayList<Vertex> getVertexes() {
         return vertexes;
     }
 
-    public void setVertexes(List<Vertex> vertexes) {
+    public void setVertexes(ArrayList<Vertex> vertexes) {
         this.vertexes = vertexes;
     }
 
-    public List<Edge> getEdges() {
+    public ArrayList<Edge> getEdges() {
         return edges;
     }
 
-    public void setEdges(List<Edge> edges) {
+    public void setEdges(ArrayList<Edge> edges) {
         this.edges = edges;
     }
 
@@ -42,10 +53,66 @@ public class Graph {
         vertexes.remove(v);
     }
 
-    private List<Edge> edges;
-    private List<Vertex> vertexes;
+    public void addSelectedEdge(Edge e){
+        selectedEdges.add(e);
+    }
+
+    public void removeSelectedEdge(Edge e){
+        selectedEdges.remove(e);
+    }
+
+    public void addSelectedVertex(Vertex v){
+        selectedVertexes.add(v);
+    }
+
+    public void removeSelectedVertex(Vertex v){
+        selectedVertexes.remove(v);
+    }
+
+    public void clearSelectedsItem(){
+        selectedEdges.clear();
+        selectedVertexes.clear();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public Color getDefaultColor() {
+        return defaultColor;
+    }
+
+    public void setDefaultColor(Color defaultColor) {
+        this.defaultColor = defaultColor;
+    }
+
+    public int getDefaultThickness() {
+        return defaultThickness;
+    }
+
+    public void setDefaultThickness(int defaultThickness) {
+        this.defaultThickness = defaultThickness;
+    }
 
     public Graph() {
+        edges = new ArrayList<Edge>();
+        selectedEdges = new ArrayList<Edge>();
+
+        vertexes = new ArrayList<Vertex>();
+        selectedVertexes = new ArrayList<Vertex>();
     }
+
 
 }
